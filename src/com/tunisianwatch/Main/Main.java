653e475012413be69ba47b73fabcec6595de0bea
marwensdiri <marwen.sdiri@esprit.tn>
2014-02-18 12:14:59 +0100

    new commit by marwen

--- src/com/tunisianwatch/Main/Main.java
+++ src/com/tunisianwatch/Main/Main.java
@@ -16,7 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("aaa");
+        System.out.println("Dev.Gear");
     }
     
 }
