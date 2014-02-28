c8043d9577f9b43b77d156869df8b1a306a2db8e
DELL <DELL@MEKNI_Aymen>
2014-02-28 20:33:49 +0100

    by tiger

--- src/com/tunisianwatch/Entities/Utilisateur.java
+++ src/com/tunisianwatch/Entities/Utilisateur.java
@@ -23,7 +23,7 @@ public class Utilisateur {
     private char sexe;
     private String adress;
     private Date dateNaissance;
-    private Blob photo;
+    private  String photo;
     private String login;
     private String mdp;
     private String mail;
@@ -38,7 +38,7 @@ public class Utilisateur {
         return nom+" "+prenom;
     }
 
-    public Utilisateur(Etablissement etablissement, String nom, String prenom, Blob photo, char sexe, String adress, String login, String mdp, String mail, char type, Date dateNaissance) {
+    public Utilisateur(Etablissement etablissement, String nom, String prenom, String photo, char sexe, String adress, String login, String mdp, String mail, char type, Date dateNaissance) {
         this.etablissement = etablissement;
         this.nom = nom;
         this.prenom = prenom;
@@ -52,7 +52,7 @@ public class Utilisateur {
         this.sexe = sexe;
     }
 
-    public Utilisateur(int id, Etablissement etablissement, String nom, String prenom, Blob photo, String login, String mdp, String mail, char type, Date dateNaissance) {
+    public Utilisateur(int id, Etablissement etablissement, String nom, String prenom, String photo, String login, String mdp, String mail, char type, Date dateNaissance) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
@@ -65,7 +65,7 @@ public class Utilisateur {
         this.etablissement = etablissement;
     }
     
-    public Utilisateur(int id,String nom, String prenom, Blob photo, String login, String mdp, String mail, char type, Date dateNaissance) {
+    public Utilisateur(int id,String nom, String prenom, String photo, String login, String mdp, String mail, char type, Date dateNaissance) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
@@ -77,7 +77,7 @@ public class Utilisateur {
         this.type = type;
     }
     
-       public Utilisateur(String nom, String prenom, Blob photo, String login, String mdp, String mail, char type, Date dateNaissance) {
+       public Utilisateur(String nom, String prenom, String photo, String login, String mdp, String mail, char type, Date dateNaissance) {
         this.id = id;
         this.nom = nom;
         this.prenom = prenom;
@@ -128,11 +128,11 @@ public class Utilisateur {
         this.dateNaissance = dateNaissance;
     }
 
-    public String getPhoto(int id) {
+    public String getPhoto() {
         return photo.toString();
     }
 
-    public void setPhoto(Blob photo) {
+    public void setPhoto(String photo) {
         this.photo = photo;
     }
 
