b5a3da5f1b42dc456ecdad26443684582ca2b90b
asd <asd@asd-PC>
2014-03-05 13:43:22 +0100

    Merge origin/master
    Conflicts:
    	src/com/tunisianwatch/Gui/InscriPanel.form
    	src/com/tunisianwatch/Gui/InscriPanel.java

--- src/com/tunisianwatch/Main/Main.java
+++ src/com/tunisianwatch/Main/Main.java
@@ -33,10 +33,9 @@ public class Main {
           
             @Override
            public void run() {
-               try {
+               
                    new Authentification().setVisible(true);
-               } catch (Exception ex) {
-               }
+               
            }
        });
         
