b39605b5e869dbacaf7f5ce71bf5809805e1dba2
DELL <DELL@MEKNI_Aymen>
2014-03-08 02:21:26 +0100

    by tiger

--- src/com/tunisianwatch/Entities/Lieu.java
+++ src/com/tunisianwatch/Entities/Lieu.java
@@ -4,36 +4,58 @@ public class Lieu {
 
     private int id;
     private String nom;
+    double lat;
+    double lon;
 
-    public Lieu(int id, String nom) {
-        this.id = id;
+    public Lieu(String nom, double lat, double lon) {
         this.nom = nom;
+        this.lat = lat;
+        this.lon = lon;
     }
 
-    public Lieu(String nom) {
-        this.nom = nom;
+    public Lieu() {
+    
     }
 
     public int getId() {
         return id;
     }
 
-    public String getNom() {
-        return nom;
-    }
-
     public void setId(int id) {
         this.id = id;
+    }
+
+    public String getNom() {
+        return nom;
     }
 
     public void setNom(String nom) {
         this.nom = nom;
     }
 
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
     @Override
     public String toString() {
-        return nom;
+        return "Lieu{" + "id=" + id + ", nom=" + nom + ", lat=" + lat + ", lon=" + lon + '}';
     }
+    
+
+    
     
     
 }
