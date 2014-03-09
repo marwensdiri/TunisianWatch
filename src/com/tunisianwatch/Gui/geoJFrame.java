58f19ec7c563e22cbae26aab87cfbe1424c42e57
DELL <DELL@MEKNI_Aymen>
2014-03-09 17:12:55 +0100

    by tiger

--- src/com/tunisianwatch/Gui/geoJFrame.java
+++ src/com/tunisianwatch/Gui/geoJFrame.java
@@ -7,7 +7,10 @@ package com.tunisianwatch.Gui;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.test.Address_components;
 import com.test.Geoloc;
+import com.test.Results;
+import com.test.Types;
 import com.tunisianwatch.Dao.LieuDao;
 import com.tunisianwatch.Entities.Geolocalisation;
 import com.tunisianwatch.Entities.Lieu;
@@ -22,6 +25,7 @@ import java.util.List;
 import java.util.StringTokenizer;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.naming.spi.DirStateFactory;
 import javax.swing.JOptionPane;
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.MapMarkerDot;
@@ -37,6 +41,7 @@ public class geoJFrame extends javax.swing.JFrame {
      */
     public static String lieu = null;
     public static Geolocalisation geo = null;
+    boolean tag = false;
 
     public geoJFrame() {
         initComponents();
@@ -53,7 +58,6 @@ public class geoJFrame extends javax.swing.JFrame {
 
         jMapViewer1 = new org.openstreetmap.gui.jmapviewer.JMapViewer();
         Map = new org.openstreetmap.gui.jmapviewer.JMapViewer();
-        Efface = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -63,28 +67,15 @@ public class geoJFrame extends javax.swing.JFrame {
             }
         });
 
-        Efface.setText("Efface");
-        Efface.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                EffaceActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(Map, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(0, 0, Short.MAX_VALUE)
-                .addComponent(Efface))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(Efface)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(Map, javax.swing.GroupLayout.PREFERRED_SIZE, 372, javax.swing.GroupLayout.PREFERRED_SIZE))
+            .addComponent(Map, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 411, Short.MAX_VALUE)
         );
 
         pack();
@@ -95,47 +86,52 @@ public class geoJFrame extends javax.swing.JFrame {
         double maplat = Map.getPosition(evt.getPoint()).getLat();
         double maplon = Map.getPosition(evt.getPoint()).getLon();
         geo = new Geolocalisation();
-       
 
-        if (MouseEvent.BUTTON1 == evt.getButton()) {
-            try {
+        if (!tag) {
+            if (MouseEvent.BUTTON1 == evt.getButton()) {
+                try {
 
-                URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + maplat + "," + maplon + "&sensor=false");
-                ObjectMapper parserMap = new ObjectMapper();
-                Geoloc geoloc = parserMap.readValue(url, Geoloc.class);
-                
-                System.out.println(geoloc.getResults().size());
-                if (geoloc.getResults().size() > 3) {
-                    lieu = geoloc.getResults().get(2).getFormatted_address();
-                    StringTokenizer str = new StringTokenizer(lieu,",");
-                    lieu=str.nextToken();
-                } else {
-                    lieu = geoloc.getResults().get(1).getFormatted_address();
-                    StringTokenizer str = new StringTokenizer(lieu,",");
-                    lieu=str.nextToken();
+                    URL url = new URL("https://maps.googleapis.com/maps/api/geocode/json?latlng=" + maplat + "," + maplon + "&sensor=false");
+                    ObjectMapper parserMap = new ObjectMapper();
+                    Geoloc geoloc = parserMap.readValue(url, Geoloc.class);
+
+                    System.out.println(geoloc.getResults().size());
+                    if (geoloc.getResults().size() > 3) {
+                        StringTokenizer str = new StringTokenizer(geoloc.getResults().get(2).getFormatted_address(), ",");
+                        lieu = str.nextToken();
+                    } else {
+                        StringTokenizer str = new StringTokenizer(geoloc.getResults().get(1).getFormatted_address(), ",");
+                        lieu = str.nextToken();
+                    }
+//                    for(Results rs : geoloc.getResults()){
+//                        for (Address_components ad : rs.getAddress_components()){
+//                            for (Types ty : ad.getTypes()){
+//                                if (ty==ty.administrative_area_level_2)
+//                                    lieu=ad.getLong_name();
+//                                else
+//                                    if(ty==ty.administrative_area_level_1)
+//                                        lieu=ad.getLong_name();
+//                            }
+//                        }
+//                    }
+
+                    MapMarkerDot map = new MapMarkerDot(lieu, new Coordinate(maplat, maplon));
+                    Map.addMapMarker(map);
+
+                    geo.setLat(maplat);
+                    geo.setLon(maplon);
+
+                } catch (MalformedURLException ex) {
+                    Logger.getLogger(geoJFrame.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (IOException ex) {
+                    Logger.getLogger(geoJFrame.class.getName()).log(Level.SEVERE, null, ex);
                 }
-
-                MapMarkerDot map = new MapMarkerDot(lieu, new Coordinate(maplat, maplon));
-                Map.addMapMarker(map);
-                
-                geo.setLat(maplat);
-                geo.setLon(maplon);
-
-            } catch (MalformedURLException ex) {
-                Logger.getLogger(geoJFrame.class.getName()).log(Level.SEVERE, null, ex);
-            } catch (IOException ex) {
-                Logger.getLogger(geoJFrame.class.getName()).log(Level.SEVERE, null, ex);
+                //tag=true;
             }
         }
     }//GEN-LAST:event_MapMouseClicked
 
-    private void EffaceActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_EffaceActionPerformed
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_EffaceActionPerformed
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Efface;
     private org.openstreetmap.gui.jmapviewer.JMapViewer Map;
     private org.openstreetmap.gui.jmapviewer.JMapViewer jMapViewer1;
     // End of variables declaration//GEN-END:variables
